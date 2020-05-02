bbcbcf570ddfec67059cd5f3ea9bfbcf0593c888
kalmodulina <mag.dziemianowicz@gmail.com>
2020-05-02 14:49:03 +0200

    zadanie 3 i 4

--- zadankaJava/src/com/company/Human.java
+++ zadankaJava/src/com/company/Human.java
@@ -6,4 +6,23 @@ public class Human {
     Phone phone;
     Animal pet;
     Car car;
+    private Double salary;
+
+    public Double getSalary(){
+        System.out.println("Your data about salary were downloaded a week ago and were equal to 4500.");
+        return salary;
+    }
+
+    public void setSalary(Double salary){
+        if(salary < 0.0)
+        {
+            throw new IllegalArgumentException("Salary must be non-negative.");
+        }
+        else {
+            System.out.println("New data has been sent to the accounting system.");
+            System.out.println("You must collect the annex to the contract from Ms. Hania.");
+            System.out.println("ZUS and US know about the salary change.");
+            this.salary = salary;
+        }
+    }
 }
