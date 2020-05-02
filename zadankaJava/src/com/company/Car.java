0e06c113dfafed670bfc6b2e2421d710978ad2a9
kalmodulina <mag.dziemianowicz@gmail.com>
2020-05-02 15:35:59 +0200

    zadanie 5

--- zadankaJava/src/com/company/Car.java
+++ zadankaJava/src/com/company/Car.java
@@ -5,8 +5,8 @@ public class Car {
     final String model;
     final Double engineSize;
     String color;
-
-
+    Double price;
+    
     public Car(String producent, String model, Double engineSize) {
         this.producent = producent;
         this.model = model;
--- zadankaJava/src/com/company/Human.java
+++ zadankaJava/src/com/company/Human.java
@@ -5,12 +5,12 @@ public class Human {
     String lastName;
     Phone phone;
     Animal pet;
-    Car car;
+    private Car car;
     private Double salary;
 
     public Double getSalary(){
         System.out.println("Your data about salary were downloaded a week ago and were equal to 4500.");
-        return salary;
+        return this.salary;
     }
 
     public void setSalary(Double salary){
@@ -23,6 +23,22 @@ public class Human {
             System.out.println("You must collect the annex to the contract from Ms. Hania.");
             System.out.println("ZUS and US know about the salary change.");
             this.salary = salary;
+        }
+    }
+
+    public Car getCar(){
+        return this.car;
+    }
+
+    public Car setCar(Car car){
+        if(this.salary > car.price){
+            System.out.println("You bought a car for cash.");
+            this.car = car;
+        } else if(this.salary > (car.price/12.0)) {
+            System.out.println("You bought a car for a credit.");
+            this.car = car;
+        } else {
+            System.out.println("Your salary is too low to buy this car.");
         }
     }
 }
